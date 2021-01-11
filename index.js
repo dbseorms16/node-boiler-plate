e1c4facbd2526c52145a2c73a801630212e56e8c
Jun seok <dbseorms16@naver.com>
2021-01-12 00:00:23 +0900

    init commit

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+const { User } = require('./model/Users');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(bodyParser.json())
 
 const mongoose = require("mongoose");
 mongoose
   .connect(
-    "mongodb+srv://dbseorms16:1234@cluster0.nciqm.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    config.mongoURI,
     {
       useNewUrlParser: true,
       useCreateIndex: true,
@@ -15,6 +23,17 @@ mongoose
   )
   .then(() => console.log("MongoDb connecting"))
   .catch((err) => console.log(err));
-app.get("/", (req, res) => res.send("hello world"));
+
+app.get("/", (req, res) => res.send("hello world112"));
+
+app.post('/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err })
+    return res.status(200).json({
+      success: true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`${port} is running`));
